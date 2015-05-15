ed90a6e8a7cf226dd31e01a5b513c4caac9f0ea1
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-05-16 00:38:12 +0200

    Added API key

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm776832545-41f9062c659812c7409d7a30', // serwer grazyna - ping
+	'm776832547-4c86a7ae5e99eefbcb18d45d', // serwer ewelina - ping
+	'm776832548-81b3b8aed77589e40c03ca66', // serwer dorota - ping
+	'm776832550-aed325fe8b7712b474d86c24', // serwer ilona - ping
+	'm776832552-c263a8110fb3e49cb6ce0c4a', // serwer helena - ping
+	'm776832554-70cc70faf2c4d0a53bf0ea35', // serwer joanna - ping
+	'm776832557-30a6ae170e41d8bbb3b61efa', // serwer klara - ping
+	'm776307966-1b7961715df43b2d6a3fc680', // informacjapubliczna.org
+	'm776307967-ca38ae71e9dc64075556cfdc', // siecobywatelska.pl
+	'm776832574-c30477774587ce95d34db26b', // watchdogportal.pl
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
