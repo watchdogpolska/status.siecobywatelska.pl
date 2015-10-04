3d0aab6823d41540a86203e6ae3ef797769d3fa6
Adam Dobrawy <ad-m@users.noreply.github.com>
2015-10-04 17:18:45 +0200

    Serwer ilona w OVH został wygaszony całkowicie

--- js/config.js
+++ js/config.js
@@ -3,7 +3,6 @@ var __apiKeys = [
 	'm776832545-41f9062c659812c7409d7a30', // serwer grazyna - ping
 	'm776832547-4c86a7ae5e99eefbcb18d45d', // serwer ewelina - ping
 	'm776832548-81b3b8aed77589e40c03ca66', // serwer dorota - ping
-	'm776832550-aed325fe8b7712b474d86c24', // serwer ilona - ping
 	'm776832552-c263a8110fb3e49cb6ce0c4a', // serwer helena - ping
 	'm776832554-70cc70faf2c4d0a53bf0ea35', // serwer joanna - ping
 	'm776832557-30a6ae170e41d8bbb3b61efa', // serwer klara - ping
