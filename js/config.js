b13d676c68c4ab42824c7a99b83f604514366482
Adam Dobrawy <ad-m@users.noreply.github.com>
2016-08-23 20:14:00 +0200

    Add rita for mailing list

--- js/config.js
+++ js/config.js
@@ -6,6 +6,7 @@ var __apiKeys = [
 	'm776832554-70cc70faf2c4d0a53bf0ea35', // serwer joanna - ping
 	'm776832557-30a6ae170e41d8bbb3b61efa', // serwer klara - ping
 	'm776913747-3e6f59b217090bc0f0edff25', // serwer laura - ping
+	'm778094606-4ea11db44ef7c2578a80f736', // serwer rita - ping
 	'm776832572-8a503b4b9120719d67e21e8a', // informacjapubliczna.org - kywd
 	'm776832571-cfb4b586fed7422c65a00692', // siecobywatelska.pl - kywd
 	'm776832574-c30477774587ce95d34db26b', // watchdogportal.pl - kywd
@@ -16,7 +17,7 @@ var __apiKeys = [
 	'm777740989-a700bbdfb9cd03028d4c3dcb', // dyski.siecobywatelska.pl - kywd
 	'm777740981-c5bb92158099261739c395db', // kontrola.siecobywatelska.pl - kywd
 	'm777860196-47e41c4b007364d4d75d18a6', // spolki.siecobywatelska.pl - kywd
-	'm777981588-d2d0f4b79eeb1d32c57c6823' // limonka.siecobywatelska.pl - kywd
+	'm777981588-d2d0f4b79eeb1d32c57c6823'  // limonka.siecobywatelska.pl - kywd
 ];
 
 // refresh interval (in seconds)
